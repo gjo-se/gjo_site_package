1ae4c520dbf15df65b8c508c6b986b620d7d1608
Gregory Jo Erdmann <gregory.jo@gjo-se.com>
2024-07-08 09:45:10 +0200

    migrate TYPO3 8 => TYPO3 12: ContentElement: added Container

---
+++ Configuration/TCA/Overrides/container.php
@@ -0,0 +1,66 @@
+<?php
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use \B13\Container\Tca\Registry;
+use \B13\Container\Tca\ContainerConfiguration;
+
+call_user_func(function()
+{
+    GeneralUtility::makeInstance(Registry::class)->configureContainer(
+        (
+        new ContainerConfiguration(
+            'container-2col',
+            '2 Column Container',
+            '2 Column Container',
+            [
+                [
+                    ['name' => 'left side', 'colPos' => 200],
+                    ['name' => 'right side', 'colPos' => 201]
+                ]
+            ]
+        )
+        )
+            ->setIcon('EXT:container/Resources/Public/Icons/container-2col.svg')
+            ->setSaveAndCloseInNewContentElementWizard(false)
+    );
+
+    GeneralUtility::makeInstance(Registry::class)->configureContainer(
+        (
+        new ContainerConfiguration(
+            'container-3col',
+            '3 Column Container',
+            '3 Column Container',
+            [
+                [
+                    ['name' => 'left side', 'colPos' => 200],
+                    ['name' => 'middle', 'colPos' => 201],
+                    ['name' => 'right side', 'colPos' => 202]
+                ]
+            ]
+        )
+        )
+            ->setIcon('EXT:container/Resources/Public/Icons/container-3col.svg')
+            ->setSaveAndCloseInNewContentElementWizard(false)
+    );
+
+    GeneralUtility::makeInstance(Registry::class)->configureContainer(
+        (
+        new ContainerConfiguration(
+            'container-4col',
+            '4 Column Container',
+            '4 Column Container',
+            [
+                [
+                    ['name' => 'left side', 'colPos' => 200],
+                    ['name' => 'middle left side', 'colPos' => 201],
+                    ['name' => 'middle right side', 'colPos' => 202],
+                    ['name' => 'right side', 'colPos' => 203]
+                ]
+            ]
+        )
+        )
+            ->setIcon('EXT:container/Resources/Public/Icons/container-4col.svg')
+            ->setSaveAndCloseInNewContentElementWizard(false)
+    );
+
+});
