b15d6e0e748b7fac9a1f0a4a48ae0d750814313e
Gregory Jo Erdmann <gregory.jo@gjo-se.com>
2024-08-13 11:26:19 +0200

    migrate TYPO3 8 => TYPO3 12: clean: Concept & File - add TCA Select Group

---
+++ Configuration/TCA/Overrides/tt_content_add_tca_select_item_group.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+call_user_func(function (): void {
+
+    $ext   = 'gjo_site_package';
+    $lll   = 'LLL:EXT:' . $ext . '/Resources/Private/Language/';
+
+    ExtensionManagementUtility::addTcaSelectItemGroup(
+        'tt_content',
+        'CType',
+        'gjose-content-elements',
+        $lll . 'ContentElements.xlf:content_element.group.gjose-content-elements',
+        'after:container'
+    );
+
+});
