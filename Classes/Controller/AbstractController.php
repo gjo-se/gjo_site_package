60b626a97ad4715b4da73e5ed32951acd7fb6894
Gregory Jo Erdmann <gregory.jo@gjo-se.com>
2024-08-20 18:48:08 +0200

    [CLEAN] MethodClean

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 
 namespace GjoSe\GjoSitePackage\Controller;
 
+use GjoSe\GjoSitePackage\Utility\CroppingUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController as CoreAbstractController;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 class AbstractController extends CoreAbstractController
 {
+    /** @param array<mixed> $additionalVariables */
+    protected function assignCommonViewVariables(array $additionalVariables = []): void
+    {
+        $this->view->assignMultiple(array_merge([
+            'data' => $this->getCurrentContentObjectData(),
+            'breakpoints' => CroppingUtility::getDefaultBreakpoints(),
+        ], $additionalVariables));
+    }
+
     /** @return array<string, mixed> */
-    protected function getCurrentContentObjectData(): array
+    private function getCurrentContentObjectData(): array
     {
         return $this->getCurrentContentObject()?->data ?? [];
     }
